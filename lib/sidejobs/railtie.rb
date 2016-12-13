fd1a7b59aeef3db320613c9d2cf67065ef2293fe
Mathías Montossi <mmontossi@buyin.io>
2016-12-13 17:25:44 -0300

    Minor improvements

--- lib/sidejobs/railtie.rb
+++ lib/sidejobs/railtie.rb
@@ -1,7 +1,7 @@
 module Sidejobs
   class Railtie < Rails::Railtie
 
-    initializer 'sidejobs.extensions' do
+    initializer 'sidejobs.active_job' do
       ::ActiveJob::QueueAdapters.include(
         Sidejobs::Extensions::ActiveJob::QueueAdapters
       )
